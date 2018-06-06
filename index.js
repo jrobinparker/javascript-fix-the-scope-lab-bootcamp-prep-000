71833279b5df78f9d47ee559030a1bf52804d643
Jeremy Parker <jrobinparker@gmail.com>
2018-06-06 16:20:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+
+describe('Fix the Scope', function() {
+  describe('myAnimal()', function() {
+    it('returns my animal', () => {
+      expect(window.myAnimal()).toEqual('dog')
+    })
+  })
+
+  describe('yourAnimal()', function() {
+    it('returns your animal', function(){
+      expect(window.yourAnimal()).toEqual('cat')
+    })
+
+    it('does not hard-code the answer', function() {
+      expect(window.yourAnimal.toString()).toNotContain("return 'cat'")
+    })
+  })
+
+  describe('add2(n)', function() {
+    it('adds two to n', function() {
+      const n = Math.floor(Math.random() * 1000)
+      expect(window.add2(n)).toEqual(n + 2)
+    })
+  })
+
+  describe('funkyFunction()', function() {
+    it('returns a function', function() {
+      expect(typeof window.funkyFunction()).toEqual('function')
+    })
+  })
+
+  describe('theFunk', function() {
+    it('is "FUNKY!"', function() {
+      expect(window.theFunk).toEqual('FUNKY!')
+    })
+  })
+
+})
+
+
+
 var animal = 'dog'
 
 function myAnimal() {
@@ -5,9 +46,6 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
   return animal
 }
 
